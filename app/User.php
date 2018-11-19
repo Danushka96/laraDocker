1a2ab86fe60c146a3468acda454b32fdcdedba66
danushka96 <danushkaherath96@gmail.com>
2018-11-19 11:06:40 +0530

    add elastic search config files

--- app/User.php
+++ app/User.php
@@ -5,10 +5,11 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Amitav\LaravelElastic\Traits\Searchable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable,Searchable;
 
     /**
      * The attributes that are mass assignable.
